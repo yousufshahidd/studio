4e1465cbce389de3598589a4cc94edf911395209
Yousuf Shahid <YousufShahidHakimYousuf@gmail.com>
2025-05-07 11:29:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <ClientPageRoot Component={function AccountDetailPage} searchParams={{}} params={{accountId:"5"}}>
    >       <AccountDetailPage params={Promise} searchParams={Promise}>
    >         <div className="flex min-h...">
    >           <main className="flex-1 md:...">
    >             <_c className="w-full max...">
    >               <div ref={null} className="rounded-lg...">
    >                 <_c2>
    >                 <_c8>
    >                   <div ref={null} className="p-6 pt-0">
    >                     <div>
    >                     <_c>
    >                       <div className="relative w...">
    >                         <table ref={null} className="w-full cap...">
    >                           <_c2>
    >                           <_c4>
    >                             <tbody ref={null} className={"[&_tr:la..."}>
    >                               <_c8 className="hover:bg-m...">
    > >                               <tr
    > >                                 ref={null}
    > >                                 className="border-b transition-colors data-[state=selected]:bg-muted hover:bg-muted/80"
    > >                               >
    >                                   <_c12>
    >                                   <_c12>
    > >                                 {" "}
    >                                   ...
    >                 ...
    >           ...
    >     ...
    >
    >
    > src/components/ui/table.tsx (59:3) @ _c8
    >
    >   57 |   React.HTMLAttributes<HTMLTableRowElement>
    >   58 | >(({ className, ...props }, ref) => (
    > > 59 |   <tr
    >      |   ^
    >   60 |     ref={ref}
    >   61 |     className={cn(
    >   62 |       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
    >
    > Call Stack
    > 19
    >
    > Show 14 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > _c8
    > src/components/ui/table.tsx (59:3)
    > <unknown>
    > src/app/account/[accountId]/page.tsx (851:24)
    > Array.map
    > <anonymous> (0:0)
    > AccountDetailPage
    > src/app/account/[accountId]/page.tsx (847:44)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -19,90 +20,103 @@ Table.displayName = "Table"
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
-))
+>(({ className, children, ...rest }, ref) => (
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...rest}>
+    {children}
+  </thead>
+));
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <tbody
     ref={ref}
     className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  />
-))
+    {...rest}
+  >
+    {children}
+  </tbody>
+));
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <tfoot
     ref={ref}
     className={cn(
       "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
       className
     )}
-    {...props}
-  />
-))
+    {...rest}
+  >
+    {children}
+  </tfoot>
+));
 TableFooter.displayName = "TableFooter"
 
-// Ensure no extraneous whitespace here to prevent hydration errors
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <tr
     ref={ref}
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props}
-  />
+    {...rest}
+  >
+    {children}
+  </tr>
 ));
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <th
     ref={ref}
     className={cn(
       "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
-    {...props}
-  />
+    {...rest}
+  >
+    {children}
+  </th>
 ))
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <td
     ref={ref}
     className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-    {...props}
-  />
+    {...rest}
+  >
+    {children}
+  </td>
 ))
 TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...rest }, ref) => (
   <caption
     ref={ref}
     className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props}
-  />
+    {...rest}
+  >
+    {children}
+  </caption>
 ))
 TableCaption.displayName = "TableCaption"
 
